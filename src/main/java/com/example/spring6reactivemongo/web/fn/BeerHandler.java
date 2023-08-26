104fbfc721a12f4c4a05378bed14c176ee8e09c6
Mohammad Reza AliAbadi <44574954+mohammadrezaaliabadi@users.noreply.github.com>
2023-08-26 18:04:56 +0330

    Beer route create close #14

--- src/main/java/com/example/spring6reactivemongo/web/fn/BeerHandler.java
+++ src/main/java/com/example/spring6reactivemongo/web/fn/BeerHandler.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
+import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Mono;
 
 @Component
@@ -13,12 +14,21 @@ import reactor.core.publisher.Mono;
 public class BeerHandler {
     private final BeerService beerService;
 
-    public Mono<ServerResponse> getBeerById(ServerRequest request){
+    public Mono<ServerResponse> createNewBeer(ServerRequest request) {
+        return beerService.saveBeer(request.bodyToMono(BeerDTO.class))
+                .flatMap(beerDTO -> ServerResponse
+                        .created(UriComponentsBuilder
+                                .fromPath(BeerRouterConfig.BEER_PATH_ID)
+                                .build(beerDTO.getId()))
+                        .build());
+    }
+
+    public Mono<ServerResponse> getBeerById(ServerRequest request) {
         return ServerResponse.ok()
                 .body(beerService.getById(request.pathVariable("beerId")), BeerDTO.class);
     }
 
-    public Mono<ServerResponse> listBeers(ServerRequest request){
+    public Mono<ServerResponse> listBeers(ServerRequest request) {
         return ServerResponse.ok()
                 .body(beerService.listBeers(), BeerDTO.class);
     }
