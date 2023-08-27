1ef761db82f2cc85241f7cb4d227436ba3477857
Mohammad Reza AliAbadi <44574954+mohammadrezaaliabadi@users.noreply.github.com>
2023-08-27 08:54:37 +0330

    Add bootstrap customer data close #29

--- src/main/java/com/example/spring6reactivemongo/bootstrap/BootstrapData.java
+++ src/main/java/com/example/spring6reactivemongo/bootstrap/BootstrapData.java
@@ -1,7 +1,9 @@
 package com.example.spring6reactivemongo.bootstrap;
 
 import com.example.spring6reactivemongo.domain.Beer;
+import com.example.spring6reactivemongo.domain.Customer;
 import com.example.spring6reactivemongo.repositories.BeerRepository;
+import com.example.spring6reactivemongo.repositories.CustomerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -14,6 +16,7 @@ import java.time.LocalDateTime;
 public class BootstrapData implements CommandLineRunner {
 
     private final BeerRepository beerRepository;
+    private final CustomerRepository customerRepository;
 
     @Override
     public void run(String... args) throws Exception {
@@ -21,6 +24,10 @@ public class BootstrapData implements CommandLineRunner {
                 .doOnSuccess(success -> {
                     loadBeerData();
                 })
+                .subscribe();
+
+        customerRepository.deleteAll()
+                .doOnSuccess(success -> loadCustomerData())
                 .subscribe();
     }
 
@@ -61,6 +68,27 @@ public class BootstrapData implements CommandLineRunner {
                 beerRepository.save(beer1).subscribe();
                 beerRepository.save(beer2).subscribe();
                 beerRepository.save(beer3).subscribe();
+            }
+        });
+    }
+
+    private void loadCustomerData() {
+        customerRepository.count().subscribe(count -> {
+            if(count == 0){
+                customerRepository.save(Customer.builder()
+                                .customerName("Customer 1")
+                                .build())
+                        .subscribe();
+
+                customerRepository.save(Customer.builder()
+                                .customerName("Customer 2")
+                                .build())
+                        .subscribe();
+
+                customerRepository.save(Customer.builder()
+                                .customerName("Customer 3")
+                                .build())
+                        .subscribe();
             }
         });
     }
